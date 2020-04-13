3da4c275785513aab0cbf4a951289aa354363734
rarress <rarressro@gmail.com>
2020-04-13 15:28:57 +0300

    Implemented camera view on takePhotoActivity

--- app/src/main/java/com/example/plantapp/takePhotoActivity.kt
+++ app/src/main/java/com/example/plantapp/takePhotoActivity.kt
@@ -1,20 +1,88 @@
 package com.example.plantapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.log.fileLogger
+import io.fotoapparat.log.logcat
+import io.fotoapparat.log.loggers
+import io.fotoapparat.parameter.ScaleType
+import io.fotoapparat.selector.back
 import kotlinx.android.synthetic.main.activity_take_photo.*
 
 class takePhotoActivity : AppCompatActivity() {
+
+    private var fotoapparat: Fotoapparat? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_take_photo)
 
         photo_button.setOnClickListener {
-            Toast.makeText(this, "photo taken", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Photo Taken", Toast.LENGTH_LONG).show()
         }
+
         gallery_button.setOnClickListener {
             Toast.makeText(this, "Enter Gallery", Toast.LENGTH_LONG).show()
         }
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 222)
+        } else {
+            initFotoapparat()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 222 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            initFotoapparat()
+            fotoapparat?.start()
+        } else {
+            Toast.makeText(
+                this,
+                "Permission for camera needed for using PlantApp",
+                Toast.LENGTH_LONG
+            ).show()
+            finish()
+        }
+    }
+
+    private fun initFotoapparat() {
+        fotoapparat = Fotoapparat(
+            context = this,
+            view = camera_view,
+            scaleType = ScaleType.CenterCrop,
+            lensPosition = back(),
+            logger = loggers(
+                logcat(),
+                fileLogger(this)
+            ),
+            cameraErrorCallback = { error -> throw error }
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        fotoapparat?.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        fotoapparat?.stop()
     }
 }
