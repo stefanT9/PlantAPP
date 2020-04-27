2a5e70dd5f82001acb7cea84d4d45e955a13c429
Mititelu Cristina <61682864+Cristina-m-29@users.noreply.github.com>
2020-04-27 18:40:02 +0300

    27 aprilie

--- app/src/main/java/com/example/plantapp/TopNavViewActivity.kt
+++ app/src/main/java/com/example/plantapp/TopNavViewActivity.kt
@@ -1,27 +1,28 @@
 package com.example.plantapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
+
 class TopNavViewActivity : AppCompatActivity(){
     private lateinit var toolbar: Toolbar
     private lateinit var navViewLayout:DrawerLayout
-    private lateinit var navView:NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_nav_view)
 
-        //cred ca aici ar veni if ul pt stabilire daca este logat sau nu si apoi setate toate variabilele
         toolbar = findViewById(R.id.topNavView)
         setSupportActionBar(toolbar)
         navViewLayout = findViewById(R.id.topNavView_layout)
-        navView = findViewById(R.id.navView)
 
         val actionBar = ActionBarDrawerToggle(
             this,
@@ -33,5 +34,6 @@ class TopNavViewActivity : AppCompatActivity(){
 
         navViewLayout.addDrawerListener(actionBar)
         actionBar.syncState()
+
     }
 }
