8663c87c198df82ea4b3cb7f65688b84f89099a5
MadalinaPlugariu <61787426+MadalinaPlugariu@users.noreply.github.com>
2020-04-29 01:38:16 +0300

    NewTests

--- app/src/androidTest/java/wikiapi/WikiapiKtTest.kt
+++ app/src/androidTest/java/wikiapi/WikiapiKtTest.kt
@@ -9,7 +9,64 @@ import org.junit.Assert.*
 class WikiapiKtTest : TestCase() {
 
     @Test
-    fun wikiapi() {
-        print(wikiapi("Floarea-soarelui"))
+    fun inputTest() {
+        try {
+            wikiapi("Floarea-soarelui")
+            wikiapi("Trandafir")
+            wikiapi("Lalea")
+            wikiapi("Mac")
+
+            throw IllegalArgumentException()
+        } catch (e: IllegalArgumentException) {
+            return
+        }
     }
+
+    @Test
+    fun outputTest(){
+    try{
+        wikiapi("Floarea-soarelui")
+        assertEquals(wikiapi("Floarea-soarelui"), "table=\n" +
+                "[Kingdom=Plantae, \n" +
+                "Clade=Tracheophytes, \n" +
+                "Clade=Angiosperms, \n" +
+                "Clade=Monocots,\n" +
+                " Order=Asparagales, \n" +
+                "Family=Asparagaceae, \n" +
+                "Subfamily=Scilloideae,\n" +
+                " Genus=Hyacinthus,\n" +
+                " Species=H. orientalis], \n" +
+                "description=Hyacinthus orientalis \n" +
+                "(common hyacinth, garden hyacinth or Dutch hyacinth),\n" +
+                " is a species of flowering plant in the family Asparagaceae, subfamily Scilloidiae,\n" +
+                " native to southwestern Asia, southern and central Turkey, northwestern Syria, \n" +
+                "Lebanon and northern Palestine. It was introduced to Europe in the 16th century.\n" +
+                " It is widely cultivated everywhere in the temperate world for its strongly fragrant \n" +
+                "flowers which appear exceptionally early in the season, and frequently forced to flower\n" +
+                " at Christmas time."))
+        throw IllegalArgumentException()
+    }
+    catch(e: IllegalArgumentException)
+    {
+        return
+    }}
+
+
+
+
+    @Test
+    fun outputNull(){
+    try{
+        wikiapi("Floarea-soarelui")
+        assertEquals(wikiapi("Floarea-soarelui", null)
+            throw NullPointerException()
+    }
+    catch(e: NullPointerException)
+    {
+        return
+    }
+    }
+
+
+
 }
