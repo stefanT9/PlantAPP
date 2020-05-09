318b541fe74c090b667ef242a9dfef0a34d3c2c7
teobalan29 <balanteodora29@gmail.com>
2020-05-09 17:32:25 +0300

    added photo taken activity

---
+++ app/src/main/java/com/example/plantapp/PhotoTakenActivity.kt
@@ -0,0 +1,32 @@
+package com.example.plantapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.content_photo_taken.*
+
+class PhotoTakenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_taken)
+
+
+
+        retakephoto.setOnClickListener(View.OnClickListener {
+
+            Toast.makeText(this@PhotoTakenActivity, "Retake photo!", Toast.LENGTH_SHORT).show()
+
+        })
+
+
+
+        seeresult.setOnClickListener(View.OnClickListener {
+
+            Toast.makeText(this@PhotoTakenActivity, "See result!", Toast.LENGTH_SHORT).show()
+
+        })
+    }
+}
+
