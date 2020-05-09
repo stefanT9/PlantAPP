5dbaa7fc78f48b661bfe5517ae07a74f586476d0
Cosmin <cosmin.aftanase@yahoo.com>
2020-05-09 22:56:34 +0300

    API integration 1/3

--- app/src/main/java/plantToTextAPI/PlantToTextAPI.kt
+++ app/src/main/java/plantToTextAPI/PlantToTextAPI.kt
@@ -1,6 +1,12 @@
 package plantToTextAPI
 
 import io.fotoapparat.result.BitmapPhoto
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
 
 /// TODO: de folosit PlantNET (Cosmin Aftanase)
 fun getPlantName(photo: BitmapPhoto?): String {
@@ -13,7 +19,38 @@ fun ocrFunction(photo: BitmapPhoto?):String{
 }
 
 /// TODO: De validat cu API Adrian (Cosmin Aftase)
-fun validatePlantLocation(latinName:String,latitude:Double,longitude:Double):Boolean
-{
-    return true
+fun validatePlantLocation(latinName: String, latitude: Double, longitude: Double): Boolean {
+
+    val params = JSONObject()
+    params.put("lat", latitude)
+    params.put("long", longitude)
+
+    var response = sendPostRequest("https://us-central1-locationip-31d6f.cloudfunctions.net/plants", params)
+
+    return response.contains(latinName)
 }
+
+fun sendPostRequest(urlName:String, params: JSONObject): String {
+    val mURL = URL(urlName)
+    with(mURL.openConnection() as HttpURLConnection) {
+        requestMethod = "POST"
+        doOutput = true;
+        doInput = true;
+        setRequestProperty("Content-Type", "application/json");
+
+        val wr = OutputStreamWriter(getOutputStream());
+        wr.write(params.toString());
+        wr.flush();
+
+        BufferedReader(InputStreamReader(inputStream)).use {
+            val response = StringBuffer()
+            var inputLine = it.readLine()
+            while (inputLine != null) {
+                response.append(inputLine)
+                inputLine = it.readLine()
+            }
+            return response.toString()
+        }
+    }
+}
+
